f28d529c7223b6d8feecbf41713f8bfd745ce728
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-10 09:00:58 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+pastebin = new PastebinAPI('BRAV-INES')
 const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
